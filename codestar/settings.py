42cb337d47e8018414ff2d3d3709d54ba669887c
Rafael Horwood sanchez <Rafaelshs@MacBook-Pro-3.local>
2025-11-14 17:26:45 +0000

    Friday evening safety commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -80,20 +80,20 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
-    "https://*.herokuapp.com"
+    "https://https://django-blog-rhs-ac633a3c544d.herokuapp.com/"
 ]
 
 # Password validation
