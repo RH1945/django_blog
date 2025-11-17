03317eae3d6a4a21363ea4951534d169a0a2a032
Rafael Horwood sanchez <Rafaelshs@MacBook-Pro-3.lan>
2025-11-17 13:25:58 +0000

    add working superuser, models

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
@@ -94,6 +94,7 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
     "https://https://django-blog-rhs-ac633a3c544d.herokuapp.com/"
+    "https://*.herokuapp.com"
 ]
 
 # Password validation
