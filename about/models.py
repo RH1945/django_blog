83aed142aeff583f23d44da260afdfac7434a221
Rafael Horwood sanchez <Rafaelshs@MacBook-Pro-3.local>
2025-11-24 15:10:42 +0000

    Add connection for about page (was not working [500 error])

--- about/models.py
+++ about/models.py
@@ -7,20 +7,21 @@ from cloudinary.models import CloudinaryField
 
 class About(models.Model):
     """
-    About model stores a single comment entry
-    related to auth user model and blog post model.
+    Stores a single about me text
     """
-    title = models.CharField(max_length=200, unique=True)
+    title = models.CharField(max_length=200)
     profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
-
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 
 class CollaborateRequest(models.Model):
+    """
+    Stores a single collaboration request message
+    """
     name = models.CharField(max_length=200)
     email = models.EmailField()
     message = models.TextField()
--- about/views.py
+++ about/views.py
@@ -9,23 +9,38 @@ from .forms import CollaborateForm
 
 def about_me(request):
     """
-    Renders the recent information about the author and collaboration request
-    model about about
+    Renders the most recent information on the website author
+    and allows user collaboration requests.
+
+    Displays an individual instance of :model:`about.About`.
+
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+        ``collaborate_form``
+            An instance of :form:`about.CollaborateForm`.
+
+    **Template**
+    :template:`about/about.html`
     """
+
     if request.method == "POST":
         collaborate_form = CollaborateForm(data=request.POST)
         if collaborate_form.is_valid():
             collaborate_form.save()
-            messages.add_message(request, messages.SUCCESS,
-                                 "Collaboration request received! I endeavour to respond within 2 working days.")
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+            )
     about = About.objects.all().order_by('-updated_on').first()
+    if about is None:
+        # Either:
+        # raise Http404("About page not yet configured")
+        # OR pass None and let the template handle it
+        pass
     collaborate_form = CollaborateForm()
-
     return render(
         request,
         "about/about.html",
-        {
-            "about": about,
-            "collaborate_form": collaborate_form
-        },
+        {"about": about, "collaborate_form": collaborate_form},
     )
